951841a4dd7a51184317765e0be2b41e84c2aecb
Ratanak <166686952+khoem-pechratanak@users.noreply.github.com>
2024-07-18 07:02:11 +0000

    finish

--- routes/index.js
+++ routes/index.js
@@ -2,69 +2,41 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
 
-
 router.get('/', function (req, res, next) {
   const userId = req.session.userid;
   const isAuth = Boolean(userId);
-  console.log(`isAuth: ${isAuth}`);
   knex("tasks")
     .select("*")
     .then(function (results) {
-      console.log(results);
       res.render('index', {
         title: 'ToDo App',
         todos: results,
+        isAuth: isAuth,
       });
     })
     .catch(function (err) {
       console.error(err);
       res.render('index', {
         title: 'ToDo App',
+        isAuth: isAuth,
       });
     });
 });
 
 router.post('/', function (req, res, next) {
-  const username = req.body.username;
-  const password = req.body.password;
-  const repassword = req.body.repassword;
-
-  knex("users")
-    .where({name: username})
-    .select("*")
-    .then(function (result) {
-      
-      if (result.length !== 0) {
-        res.render("signup", {
-          title: "Sign up",
-          errorMessage: ["このユーザ名は既に使われています"],
-        }) 
-      } else if (password === repassword) {
-        
-        knex("users")
-          .insert({name: username, password: password})
-          .then(function () {
-            res.redirect("/");
-          })
-          .catch(function (err) {
-            console.error(err);
-            res.render("signup", {
-              title: "Sign up",
-              errorMessage: [err.sqlMessage],
-            });
-          });
-      } else {
-        res.render("signup", {
-          title: "Sign up",
-          errorMessage: ["パスワードが一致しません"],
-        });
-      }
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
+  const todo = req.body.add;
+  knex("tasks")
+    .insert({user_id: 1, content: todo})
+    .then(function () {
+      res.redirect('/')
     })
     .catch(function (err) {
       console.error(err);
-      res.render("signup", {
-        title: "Sign up",
-        errorMessage: [err.sqlMessage],
+      res.render('index', {
+        title: 'ToDo App',
+        isAuth: isAuth,
       });
     });
 });
